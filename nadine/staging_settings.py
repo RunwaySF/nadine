9f9a3e8e634fb71618ea584e045166d67ece3b34
Joe Vasquez <joevasquez@Joes-MacBook-Air-2.local>
2017-06-24 10:59:14 -0700

    Updates to allowed hosts for staging.

--- nadine/staging_settings.py
+++ nadine/staging_settings.py
@@ -6,7 +6,11 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 path = lambda *a: os.path.join(ROOT, *a)
 
 PRODUCTION = False
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = (
+  'runway-nadine.herokuapp.com'
+)
+
 
 # Admins
 ADMINS = (
